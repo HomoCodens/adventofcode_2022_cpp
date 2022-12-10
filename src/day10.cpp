1ddbf47d9b817cea0d833ebaba3193d6e96134ef
HomoCodens <homocodens@github.com>
2022-12-10 11:38:18 +0100

    day10 part2
    only feeling a *little* dirty

--- src/day10.cpp
+++ src/day10.cpp
@@ -17,8 +17,10 @@ namespace day10 {
         int x{1};
         int cycle{0};
         int signalStrength{0};
+        string screen(240, '.');
+
         string line;
-        while(!f.eof() && (cycle <= 222)) {
+        while(!f.eof()) {
             instruction = "";
             y = 0;
             std::getline(f, line);
@@ -30,13 +32,30 @@ namespace day10 {
             
             if(line != "") {
                 if(instruction == "noop") {
+                    // part 2
+                    if(std::abs((cycle % 40) - x) <= 1) {
+                        screen[cycle] = '#';
+                    }
+
                     ++cycle;
+
+                    // part 1 shtoff
                     if((cycle - 20) % 40 == 0) {
                         signalStrength += cycle*x;
                     }
+
                 } else {
+                    // part2
+                    for(int i = 0; i < 2; ++i) {
+                        if(std::abs(((cycle + i) % 40) - x) <= 1) {
+                            screen[cycle + i] = '#';
+                        }
+                    }
+
                     cycle += 2;
                     x += y;
+
+                    // part1
                     if((cycle - 20) % 40 == 0) {
                         // x is still on the previous value DURING the cycle
                         signalStrength += cycle*(x - y);
@@ -48,8 +67,17 @@ namespace day10 {
             }
         }
 
+        std::ostringstream os{};
+        for(int i = 0; i < 240; ++i) {
+            if(i > 0 && (i % 40) == 0) {
+                os << '\n';
+            }
+            os << screen[i];
+        }
+
         return DayResults{
-            std::to_string(signalStrength)
+            "The total signal strength at the checkpoints is " + std::to_string(signalStrength),
+            "Read for yourself, meatbag: \n" + os.str()
         };
     }
 }
